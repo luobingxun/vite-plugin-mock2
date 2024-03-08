671b769d2cccb01939443f92d3a0f01b07dc0adb
mingwei.li <mingwei.li@tarsocial.com>
2024-03-08 17:18:39 +0800

    feat: test

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mock from '../src/index';
+// import mock from '../src/index';
+import mock from 'vite-plugin-mock3';
 
 export default defineConfig({
   plugins: [mock(), react()]
